43412e8db7b658aaf1d210fcb94b17ceef8932f2
kdh0427 <166967751+kdh0427@users.noreply.github.com>
2025-05-08 14:57:35 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/ayen/AyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ayen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
